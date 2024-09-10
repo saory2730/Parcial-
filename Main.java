21e7a0f7601d24676e48ed5a53c953aaa496fd9d
saory2730 <161676850+saory2730@users.noreply.github.com>
2024-09-09 20:53:26 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,19 +11,19 @@ System.out.println();
 System.out.println();
 
 libro1.libro("El coronel no tiene quien le escriba", "Gabriel Garcia");
-libro1.imprimir("El coronel no tiene quien le escriba", "Gabriel Garcia", "Kate", 45000);
+libro1.imprimir("El coronel no tiene quien le escriba", "Gabriel Garcia", "Katerin cortes", 45000);
  System.out.println();
    LibroTexto libro2 =new LibroTexto();
-   libro2.libroTexto("Cien años de soledad", "Gabriel Garcia", "Juan", 67000, "b411");
+   libro2.libroTexto("Cien años de soledad", "Gabriel Garcia", "Katerin cortes", 67000, "b411");
 System.out.println();
 
 
    LibroTextoUniajc libro3=new LibroTextoUniajc();
    
-libro3.LibroTextoUniajc("Sara tomate", "Tom", "Alejandra", 76000, "C421", "Salud");
+libro3.LibroTextoUniajc("Sara tomate", "Tom", "Katerin cortes", 76000, "C421", "Salud");
 System.out.println();
    Novela libro4= new Novela();
-   libro4.novela("Satanas", "Andres", "Eliana", 80000, "B311", "Ingenieria","Policiaca");
+   libro4.novela("Satanas", "Andres", "Katerin cortes", 80000, "B311", "Ingenieria","Policiaca");
     }
 
 
